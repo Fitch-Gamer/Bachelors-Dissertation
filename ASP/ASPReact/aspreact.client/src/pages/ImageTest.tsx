d5dea31c144d301960dbbd5d1c09f0219d8fb01e
Finley-Hitchings <fitchings@outlook.com>
2026-04-22 17:28:09 +0100

    image update

--- ASP/ASPReact/aspreact.client/src/pages/ImageTest.tsx
+++ ASP/ASPReact/aspreact.client/src/pages/ImageTest.tsx
@@ -1,12 +1,11 @@
 import useDocumentTitle from '../hooks/useDocumentTitle';
-import imageSrc from '../../public/perturb_full_x.jpg';
 
 export default function ImageTest() {
     useDocumentTitle('ASPReact');
 
     return (
         <div>
-            <img src={imageSrc} alt="Image" />
+            <img src="/perturb_full_X.jpg" alt="Image" />
             <p><a href="/">Back to home</a></p>
         </div>
     );
